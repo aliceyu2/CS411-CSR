05996ce411238b5cd711f6e96ffe8f7290919180
Alice Yu <aliceyu2@illinois.edu>
2019-03-09 12:58:14 -0600

    Homepage

---
+++ mySite/mySearch/template/action_page.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+
+<a href="homepage2.html">Go back to the homepage</a>
+
+</body>
+</html>
